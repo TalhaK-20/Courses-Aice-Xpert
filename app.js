56e50cee4f2d0b593969a4cf3b8d88bad9c81b2a
TalhaK-20 <F2021266625@umt.edu.pk>
2025-01-28 19:21:08 +0500

    Errors are Resolved

--- app.js
+++ app.js
@@ -58,11 +58,13 @@ app.use(session({
 }));
 
 
-const storage = multer.memoryStorage();
-
-upload = multer({
-    storage: storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5 MB (optional)
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
 });
 
 
